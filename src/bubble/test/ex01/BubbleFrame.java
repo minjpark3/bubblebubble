a060954b4589052712ec75ca4e2867779e5bc4bc
minjpark3 <minj9850610@gmail.com>
2022-09-26 23:46:59 +0900

    feat : 캐릭터 좌우이동 구현중

--- src/bubble/test/ex01/BubbleFrame.java
+++ src/bubble/test/ex01/BubbleFrame.java
@@ -1,5 +1,9 @@
 package bubble.test.ex01;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,25 +18,44 @@ public class BubbleFrame extends JFrame{
 	public BubbleFrame() {
 		initObject();
 		initSetting();
+		initListener();
 		setVisible(true);//그림을 그려라
 	
 	}
 	private void initObject() {
 		backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
 		setContentPane(backgroundMap);//3개층을 만들필요가 없어짐
-		
 		player = new Player();
 		add(player);
-//		backgroundMap.setSize(1000,600);
-//		//backgroundMap.setLocation(300,300);
-//		add(backgroundMap);//JFrame에 JLabel이 그려진다.
 	}
+
 	private void initSetting() {
 		setSize(1000,640);
 		setLayout(null); //absoulte 레이아웃(자유롭게 그림을 그릴 수 있다)
+	
 		setLocationRelativeTo(null);//JFrame 가운데 배치하기 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //x버튼 으로 창끌때 JVM같이 종료
 		
+	}
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println(e.getKeyCode());
+			
+			switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.left();
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.right();
+					break;
+				case KeyEvent.VK_UP:
+					player.up();
+					break;
+				}
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
--- src/bubble/test/ex01/Player.java
+++ src/bubble/test/ex01/Player.java
@@ -3,29 +3,68 @@ package bubble.test.ex01;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class Player extends JLabel{
+//class Player ->new 가능한 애들! 게임에 존재할 수 있음.(추상메서드 가질수 없다.)
+public class Player extends JLabel implements Moveable {
 
+	//위치상태
 	private int x;
 	private int y;
 
-	private ImageIcon playerR,playerL;
+	//움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
 	
+	private ImageIcon playerR, playerL;
+
 	public Player() {
 		initObject();
 		initSetting();
 	}
+
 	private void initObject() {
 		playerR = new ImageIcon("image/playerR.png");
 		playerL = new ImageIcon("image/playerL.png");
-		
+
 	}
-	
+
 	private void initSetting() {
-		x = 70;
+		x = 55;
 		y = 535;
 		
+		left = false;
+		right =false;
+		up = false;
+		down = false;
+		
+
 		setIcon(playerR);
-		setSize(50,50);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+	@Override
+	public void left() {
+		setIcon(playerL);
+		x = x - 10;
 		setLocation(x,y);
+	}
+
+	@Override
+	public void right() {
+		setIcon(playerR);
+		x = x + 10;
+		setLocation(x,y);
+	}
+
+	@Override
+	public void up() {
+
+	}
+
+	@Override
+	public void down() {
+
 	}
 }
